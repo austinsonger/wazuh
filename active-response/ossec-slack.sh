dac5c43ff77fe6377d93f7671dae2e261665f2c6
Austin Songer <austinvernsonger@protonmail.com>
2019-12-29 11:19:12 -0600

    Upgrading Legacy Syntax
    Would these changes provide the same outcome they should.

--- active-response/ossec-slack.sh
+++ active-response/ossec-slack.sh
@@ -24,30 +24,30 @@ if [ "x$1" = "xdelete" ]; then
 fi
 ALERTID=$4
 RULEID=$5
-LOCAL=`dirname $0`;
-ALERTTIME=`echo "$ALERTID" | cut -d  "." -f 1`
-ALERTLAST=`echo "$ALERTID" | cut -d  "." -f 2`
+LOCAL=$(dirname "$0");
+ALERTTIME=$(echo "$ALERTID" | cut -d  "." -f 1)
+ALERTLAST=$(echo "$ALERTID" | cut -d  "." -f 2)
 
 # Logging
-cd $LOCAL
+cd "$LOCAL" || exit
 cd ../
-PWD=`pwd`
-echo "`date` $0 $1 $2 $3 $4 $5 $6 $7 $8" >> ${PWD}/../logs/active-responses.log
-ALERTFULL=`grep -A 10 "$ALERTTIME" ${PWD}/../logs/alerts/alerts.log | grep -v "\.$ALERTLAST: " -A 10 | grep -v "Src IP: " | grep -v "User: " |grep "Rule: " -A 4 | cut -c -139 | sed 's/\"//g'`
+PWD=$(pwd)
+echo "$(date) $0 $1 $2 $3 $4 $5 $6 $7 $8" >> "${PWD}"/../logs/active-responses.log
+ALERTFULL=$(grep -A 10 "$ALERTTIME" "${PWD}"/../logs/alerts/alerts.log | grep -v "\.$ALERTLAST: " -A 10 | grep -v "Src IP: " | grep -v "User: " |grep "Rule: " -A 4 | cut -c -139 | sed 's/\"//g')
 
 PAYLOAD='{"channel": "'"$CHANNEL"'", "username": "'"$SLACKUSER"'", "text": "'"${ALERTFULL}"'"}'
 
-ls "`which curl`" > /dev/null 2>&1
+ls "$(which curl)" > /dev/null 2>&1
 if [ ! $? = 0 ]; then
-    ls "`which wget`" > /dev/null 2>&1
+    ls "$(which wget)" > /dev/null 2>&1
     if [ $? = 0 ]; then
-        wget --keep-session-cookies --post-data="${PAYLOAD}" ${SITE} 2>>${PWD}/../logs/active-responses.log
+        wget --keep-session-cookies --post-data="${PAYLOAD}" "${SITE}" 2>>"${PWD}"/../logs/active-responses.log
         exit 0;
     fi
 else
-    curl -s -X POST --data-urlencode "payload=${PAYLOAD}" ${SITE} 2>>${PWD}/../logs/active-responses.log
+    curl -s -X POST --data-urlencode "payload=${PAYLOAD}" "${SITE}" 2>>"${PWD}"/../logs/active-responses.log
     exit 0;
 fi
 
-echo "`date` $0: Unable to find curl or wget." >> ${PWD}/../logs/active-responses.log
+echo "$(date) $0: Unable to find curl or wget." >> "${PWD}"/../logs/active-responses.log
 exit 1;
