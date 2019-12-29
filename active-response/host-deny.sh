ec35fa09346765a51b1faabfdc05e55d20d67d84
Austin Songer <austinvernsonger@protonmail.com>
2019-12-29 11:26:19 -0600

    Updated Legacy Chell

--- active-response/host-deny.sh
+++ active-response/host-deny.sh
@@ -10,13 +10,13 @@ ACTION=$1
 USER=$2
 IP=$3
 
-LOCAL=`dirname $0`;
-cd $LOCAL
+LOCAL=$(dirname "$0");
+cd "$LOCAL" || exit
 cd ../
-PWD=`pwd`
+PWD=$(pwd)
 LOCK="${PWD}/host-deny-lock"
 LOCK_PID="${PWD}/host-deny-lock/pid"
-UNAME=`uname`
+UNAME=$(uname)
 
 
 # This number should be more than enough (even if a hundred
@@ -31,35 +31,35 @@ lock()
     i=0;
     # Providing a lock.
     while [ 1 ]; do
-        mkdir ${LOCK} > /dev/null 2>&1
+        mkdir "${LOCK}" > /dev/null 2>&1
         MSL=$?
         if [ "${MSL}" = "0" ]; then
             # Lock acquired (setting the pid)
-            echo "$$" > ${LOCK_PID}
+            echo "$$" > "${LOCK_PID}"
             return;
         fi
 
         # Getting currently/saved PID locking the file
-        C_PID=`cat ${LOCK_PID} 2>/dev/null`
+        C_PID=$(cat "${LOCK_PID}" 2>/dev/null)
         if [ "x" = "x${S_PID}" ]; then
             S_PID=${C_PID}
         fi
 
         # Breaking out of the loop after X attempts
         if [ "x${C_PID}" = "x${S_PID}" ]; then
-            i=`expr $i + 1`;
+            i=$(expr $i + 1);
         fi
 
-        sleep $i;
+        sleep "$i";
 
-        i=`expr $i + 1`;
+        i=$(expr "$i" + 1);
 
         # So i increments 2 by 2 if the pid does not change.
         # If the pid keeps changing, we will increments one
         # by one and fail after MAX_ITERACTION
         if [ "$i" = "${MAX_ITERATION}" ]; then
-            echo "`date` Unable to execute. Locked: $0" \
-                        >> ${PWD}/ossec-hids-responses.log
+            echo "$(date) Unable to execute. Locked: $0" \
+                        >> "${PWD}"/ossec-hids-responses.log
 
             # Unlocking and exiting
             unlock;
@@ -71,12 +71,12 @@ lock()
 # Unlock function
 unlock()
 {
-    rm -rf ${LOCK}
+    rm -rf "${LOCK}"
 }
 
 
 # Logging the call
-echo "`date` $0 $1 $2 $3 $4 $5" >> ${PWD}/../logs/active-responses.log
+echo "$(date) $0 $1 $2 $3 $4 $5" >> "${PWD}"/../logs/active-responses.log
 
 
 # IP Address must be provided
@@ -89,7 +89,7 @@ fi
 # Checking for invalid entries (lacking "." or ":", etc)
 echo "${IP}" | egrep "\.|\:" > /dev/null 2>&1
 if [ ! $? = 0 ]; then
-    echo "`date` Invalid ip/hostname entry: ${IP}" >> ${PWD}/../logs/active-responses.log
+    echo "$(date) Invalid ip/hostname entry: ${IP}" >> "${PWD}"/../logs/active-responses.log
     exit 1;
 fi
 
@@ -99,7 +99,7 @@ if [ "x${ACTION}" = "xadd" ]; then
     # Looking for duplication
     IPKEY=$(grep -w "${IP}" /etc/hosts.deny)
     if [ ! -z "$IPKEY" ]; then
-        echo "IP ${IP} already exists on host.deny..." >> ${PWD}/../logs/active-responses.log
+        echo "IP ${IP} already exists on host.deny..." >> "${PWD}"/../logs/active-responses.log
         exit 1
     fi
     lock;
@@ -119,22 +119,22 @@ if [ "x${ACTION}" = "xadd" ]; then
 # Deleting from hosts.deny
 elif [ "x${ACTION}" = "xdelete" ]; then
     lock;
-    TMP_FILE=`mktemp ${PWD}/ossec-hosts.XXXXXXXXXX`
+    TMP_FILE=$(mktemp "${PWD}"/ossec-hosts.XXXXXXXXXX)
     if [ "X${TMP_FILE}" = "X" ]; then
         # Cheap fake tmpfile, but should be harder then no random data
-        TMP_FILE="${PWD}/ossec-hosts.`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -1 `"
+        TMP_FILE="${PWD}/ossec-hosts.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -1 )"
     fi
     echo "${IP}" | grep "\:" > /dev/null 2>&1
     if [ $? = 0 ]; then
         IP="\[${IP}\]"
     fi
     if [ "X$UNAME" = "XFreeBSD" ]; then
-        cat /etc/hosts.allow | grep -v "ALL : ${IP} : deny$"> ${TMP_FILE}
-        mv ${TMP_FILE} /etc/hosts.allow
+        cat /etc/hosts.allow | grep -v "ALL : ${IP} : deny$"> "${TMP_FILE}"
+        mv "${TMP_FILE}" /etc/hosts.allow
     else
-        cat /etc/hosts.deny | grep -v "ALL:${IP}$"> ${TMP_FILE}
-        cat ${TMP_FILE} > /etc/hosts.deny
-        rm ${TMP_FILE}
+        cat /etc/hosts.deny | grep -v "ALL:${IP}$"> "${TMP_FILE}"
+        cat "${TMP_FILE}" > /etc/hosts.deny
+        rm "${TMP_FILE}"
     fi
     unlock;
     exit 0;
